00e0c66b9ad512192dfa6bf7cd7ce947149d2f18
Richard C Gerkin <rick1@gerkin.org>
2018-03-02 13:11:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ setup(
             'owtests = owtests.__main__:main'
             ]
         },
-    dependency_links = ["git+https://github.com/scidash/sciunit#egg=sciunit-0.1.9",
-                        "git+https://github.com/scidash/neuronunit#egg=neuronunit-0.1.9",
+    dependency_links = ["git+https://github.com/scidash/sciunit#egg=sciunit-0.19",
+                        "git+https://github.com/scidash/neuronunit#egg=neuronunit-0.19",
                         "git+https://github.com/openworm/CElegansNeuroML@sciunit#egg=CElegansNeuroML"]
     )
 
