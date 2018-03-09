93646969967163bd523ef8e5e23aad3013c69645
Russell Jarvis <rjjarvis@asu.edu>
2018-03-09 16:27:35 -0700

    removed cruft from staging area before pull request

--- install_shell.sh
+++
@@ -1,8 +0,0 @@
-export OPENWORM_HOME=`pwd`
-echo $OPENWORM_HOME
-sudo /opt/conda/bin/pip install -e . --process-dependency-links
-# Comment out clones when done
-git clone https://github.com/openworm/ChannelWorm.git
-#
-git clone https://github.com/openworm/CElegansNeuroML.git
-sudo /opt/conda/bin/pip install -e . --process-dependency-links
